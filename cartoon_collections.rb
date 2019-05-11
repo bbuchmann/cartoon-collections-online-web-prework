f8415158c62f7bc6c59ed7299f8a8ec4802d134d
Ben Buchmann <ben_buchy92@hotmail.com>
2019-05-11 06:50:55 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,6 +1,4 @@
-def Roll_call_dwarves = Roll_call_dwarves.new ["Doc","Dopey","Bashful","Grumpy"]
-  
-  .each_with_index { |item,index|
+def Roll_call_dwarves = Roll_call_dwarves.new ["Doc","Dopey","Bashful","Grumpy"].each_with_index { |item,index|
 
 
 end
