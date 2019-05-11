6fb71f0132abbf8af4d8a59916375e810bee5edf
Ben Buchmann <ben_buchy92@hotmail.com>
2019-05-11 07:07:06 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -8,7 +8,7 @@ def Roll_call_dwarves = Roll_call_dwarves.new
 
 end
 
-def Summon_captain_planet
+def Summon_captain_planet = ["puff","go","two"]
 end
 
 def long_planeteer_calls# code an argument here
