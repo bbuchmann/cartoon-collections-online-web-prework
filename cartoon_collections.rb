19186515ad845c3b78c3917bb48056227f04a776
Ben Buchmann <ben_buchy92@hotmail.com>
2019-05-11 06:36:06 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,6 +1,6 @@
 def Roll_call_dwarves = ["Doc","Dopey","Bashful","Grumpy"]
   Hash = Hash.New 
-  %w (Doc,Dopey,Bashful,Grumpy).each_with_index {
+  %w (Doc,Dopey,Bashful,Grumpy).each_with_index { |
 
 end
 
